e6075e5ee328340604cf6205ade9c87ebbaf43b2
Sara <sara.saezdecortazar.52977@ikasle.egibide.org>
2025-10-29 11:50:01 +0100

    menu2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         menu();
+        menu2();
         }
     public static void menu(){
-        System.out.println("1. Rama Dos");
+        System.out.println("1. Rama Uno");
+    }
+    public static void menu2(){
+        System.out.println("2. Rama Dos");
     }
 }
